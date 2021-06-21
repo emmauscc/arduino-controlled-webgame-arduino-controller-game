5184d261b34d1884183c808ff61b7c903ff153d1
Christopher EVANS <christopher.evans@students.emmauscc.sa.edu.au>
2021-06-21 14:27:43 +0930

    Base functionality almost completely finished

---
+++ CETest/CETest.js
@@ -0,0 +1,311 @@
+$(document).ready(function(){
+    /*for(var i=1;i<4;i++){
+        $('body').append('<div></div>').children().last().addClass('row').attr('id', 'row'+i);
+    }    
+    for(var j=1;j<4;j++){
+        for(var i=0;i<30;i++){
+            if(j==2){
+                if(i==0){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('top').addClass('left').addClass('grid').addClass('gridStyle');
+                }else if(i>0 && i<9){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('top').addClass('grid').addClass('gridStyle');
+                }else if(i==9){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('right').addClass('grid').addClass('gridStyle');
+                }else if(i==10){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('left').addClass('grid').addClass('gridStyle');
+                }else if(i>10 && i<19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('middle').addClass('grid').addClass('gridStyle');
+                }else if(i==19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('right').addClass('grid').addClass('gridStyle');
+                }else if(i==20){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('left').addClass('grid').addClass('gridStyle');
+                }else if(i>20 && i<29){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('bottom').addClass('grid').addClass('gridStyle');
+                }else if(i==29){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('bottom').addClass('right').addClass('grid').addClass('gridStyle');
+                }
+            }else if(j==1){
+                if(i==0){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('left').addClass('grid').addClass('gridStyle').addClass('gridStyle');
+                }else if(i>0 && i<9){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('top').addClass('grid').addClass('gridStyle');
+                }else if(i==9){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('top').addClass('right').addClass('grid').addClass('gridStyle');
+                }else if(i==10){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('player').addClass('grid').addClass('gridStyle');
+                }else if(i>10 && i<19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('middle').addClass('grid').addClass('gridStyle');
+                }else if(i==19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('right').addClass('grid').addClass('gridStyle');
+                }else if(i==20){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('bottom').addClass('left').addClass('grid').addClass('gridStyle');
+                }else if(i>20 && i<29){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('bottom').addClass('grid').addClass('gridStyle');
+                }else if(i==29){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('right').addClass('grid').addClass('gridStyle');
+                }
+            }else{
+                if(i==0){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('left').addClass('grid').addClass('gridStyle');
+                }else if(i>0 && i<9){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('top').addClass('grid').addClass('gridStyle');
+                }else if(i==9){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('top').addClass('right').addClass('grid').addClass('gridStyle');
+                }else if(i==10){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('left').addClass('grid').addClass('gridStyle');
+                }else if(i>10 && i<19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('middle').addClass('grid').addClass('gridStyle');
+                }else if(i==19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('right').addClass('grid').addClass('gridStyle');
+                }else if(i==20){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('bottom').addClass('left').addClass('grid').addClass('gridStyle');
+                }else if(i>20 && i<29){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('bottom').addClass('grid').addClass('gridStyle');
+                }else if(i==29){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).addClass('right').addClass('grid').addClass('gridStyle');
+                }//else{
+                //  $('#row'+j).append('<div></div>').children().last().attr('gridNum', +i).attr('rowNum', j);
+                //}
+                
+            }
+        }
+    }
+    /*function keyPress(e){
+        if(e.key=="a"){
+            if($(".player").attr('gridNum')==0){
+
+            }else if($(".player").attr('gridNum')==10){
+
+            }else if($(".player").attr('gridNum')==20){
+
+            }else{
+                x=$('.player').attr('gridNum');
+                if(x<9 && x>0){
+                    $('.player').removeClass().addClass('top');
+                }else if(x==19){
+                    $('.player').removeClass().addClass('holeRight');
+                }else if(x<19 && x>10){
+                    $('.player').removeClass().addClass('middle');
+                }else if(x<29 && x>20){
+                    $('.player').removeClass().addClass('bottom');
+                }else if(x==29){
+                    $('.player').removeClass().addClass('holeRight');
+                }
+                x=x-1;
+                $('.player').attr('gridNum', x);
+            }
+        }
+    }*/
+    for(var i=1;i<3;i++){
+        $('body').append('<div></div>').children().last().addClass('rowBorder').attr('id', 'row'+i);
+    }
+    $('body').append('<div></div>').children().last().addClass('lives');
+    for(var i=0;i<3;i++){
+        $('.lives').append('<div></div>').children().last().addClass('alive').attr('life', i);
+    }
+    /*for(var i=0;i<30;i++){
+        if(i>=0 && i<10){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', '1').addClass('grid').addClass('empty');
+        }else if(i==10){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', '1').addClass('player');
+        }else{
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', '1').addClass('grid').addClass('empty');
+        }
+    }*/
+
+    for(var j=1;j<3;j++){
+        for(var i=0;i<30;i++){
+            if(j==1){
+                if(i<2){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }else if(i==2){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'obstacle').addClass('grid').addClass('obstacle');
+                }else if(i>2 && i<10){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }else if(i==10){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('player');
+                }else if(i>10 && i<12){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }else if(i==12){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'obstacle').addClass('grid').addClass('obstacle');
+                }else if(i>12 && i<14){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }else if(i==14){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'obstacle').addClass('grid').addClass('obstacle');
+                }else if(i>14 && i<19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }else if(i==19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'hole').addClass('grid').addClass('gridStyle');
+                }else if(i>19 && i<24){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }else if(i==24){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'obstacle').addClass('grid').addClass('obstacle');
+                }else if(i>24){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }
+            }else if(j % 2==0){
+                if(i<10){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }else if(i==10){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'hole').addClass('grid').addClass('gridStyle');
+                }else if(i>10){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }
+            }else if(j % 2!=0){
+                if(i<19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }else if(i==19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'hole').addClass('grid').addClass('gridStyle');
+                }else if(i>19){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', j).attr('status', 'empty').addClass('grid').addClass('gridStyle');
+                }
+            }
+        }
+    }
+    /*for(var i=0;i<30;i++){
+        if(i==0){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('holeLeft').addClass('grid');
+        }else if(i>0 && i<9){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('top').addClass('grid');
+        }else if(i==9){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('topRight').addClass('grid');
+        }else if(i==10){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('player').addClass('grid');
+        }else if(i>10 && i<19){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('middle').addClass('grid');
+        }else if(i==19){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('holeRight').addClass('grid');
+        }else if(i==20){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('bottomLeft').addClass('grid');
+        }else if(i>20 && i<29){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('bottom').addClass('grid');
+        }else if(i==29){
+            $('#row1').append('<div></div>').children().last().attr('gridNum', i).attr('rowNum', 1).addClass('holeRight').addClass('grid');
+        }
+    }
+
+    /*for(var j=1;j<4;j++){
+        for(var i=0;i<30;i++){
+            if(j==2){
+                if(i==20){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', +i).attr('rowNum', j);
+                }else if(i==9){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', +i).attr('rowNum', j);
+                }else{
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', +i).attr('rowNum', j);
+                }
+            }else{
+                if(i==0){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', +i).attr('rowNum', j);
+                }else if(i==29){
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', +i).attr('rowNum', j);
+                }else{
+                    $('#row'+j).append('<div></div>').children().last().attr('gridNum', +i).attr('rowNum', j);
+                }
+            }
+        }
+    }*/
+    
+
+    /*for(var i=0; i<39; i++){
+        for(var j=0;j<3;j++){
+            if(i==10){
+                if($('#grid'+i).attr('rowNum', j)>0){
+                    $('#grid'+i).css('float', 'none');
+                }
+            }else if(i==20){
+                if($('#grid'+i).attr('rowNum', j)>0){
+                    $('#grid'+i).css('float', 'none');
+                }
+            }else if(i==30){
+                if($('#grid'+i).attr('rowNum', j)>0){
+                    $('#grid'+i).css('float', 'none');
+                }
+            }else{
+    
+            }
+        }
+    }*/
+});
+var x;
+var y;
+var lifeNum=0;
+function statusCheck(){
+    setTimeout(function(){
+        if($('.player').attr('status')=="hole"){
+            y=y+1
+            $('.player').removeClass().addClass('gridStyle').addClass('grid');
+            $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+        }else if($('.player').attr('status')=="obstacle"){
+            $('.player').removeClass().addClass('obstacle').addClass('grid');
+            x=x-1;
+            $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+            $('.alive[life='+lifeNum+']').removeClass().addClass('dead');
+            lifeNum=lifeNum+1;
+        }else{
+
+        }
+    }, 1000);
+}
+function keyIn(e){
+    x=parseInt($('.player').attr('gridNum'));
+    y=parseInt($('.player').attr('rowNum'));
+        if(e.key=="a"){
+            if(x==0){
+
+            }else if(x==10){
+
+            }else if(x==20){
+
+            }else{
+                $('.player').removeClass().addClass('gridStyle').addClass('grid');
+                x=x-1;
+                $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+                statusCheck();
+            }
+        }else if(e.key=="d"){
+            if(x==9){
+
+            }else if(x==19){
+
+            }else if(x==29){
+
+            }else{
+                $('.player').removeClass().addClass('gridStyle').addClass('grid');
+                x=x+1;
+                $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+                statusCheck();
+            }
+        }else if(e.key=="w"){
+            if(x>=0 && x<=9){
+
+            }else if(x>=20 && x<=29){
+                $('.player').removeClass().addClass('gridStyle').addClass('grid');
+                    x=x-10;
+                    $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+                    statusCheck();
+            }else{
+                $('.player').removeClass().addClass('gridStyle').addClass('grid');
+                x=x-10;               
+                $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+                setTimeout(function(){
+                    $('.player').removeClass().addClass('gridStyle').addClass('grid');
+                    x=x+10;                  
+                    $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+                }, 1000);
+                statusCheck();
+            }
+        }else if(e.key=="s"){
+            if(x<20){
+                $('.player').removeClass().addClass('gridStyle').addClass('grid');
+                x=x+10;
+                $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+                statusCheck();
+            }else if(x>=20 && x<=29){
+                $('.player').removeClass().addClass('gridStyle').addClass('grid');
+                x=x-10;
+                $('#row'+y).children('.grid[gridNum='+x+']').removeClass().addClass('player');
+                statusCheck();
+            }
+        }
+    }
