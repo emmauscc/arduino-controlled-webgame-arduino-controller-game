f332655733afaffa207fc71e8917525434a9e258
Jacob FONTANAROSA <jacob.fontanarosa@students.emmauscc.sa.edu.au>
2021-06-22 12:51:01 +0930

    LED Button Commit

--- Lives_System/Lives_System.ino
+++ Lives_System/Lives_System.ino
@@ -1,12 +1,43 @@
 void setup() {
-  pinMode(9, OUTPUT);
   
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(3, INPUT);
 }
 
 void loop() {
+
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(9, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(9, LOW);
+  }
+  
   digitalWrite(9, HIGH);
   delay(1000);
   digitalWrite(9, LOW);
+  delay(1000);
+
+  digitalWrite(10, HIGH);
+  delay(1000);
+  digitalWrite(10, LOW);
+  delay(1000);
+
+  digitalWrite(11, HIGH);
+  delay(1000);
+  digitalWrite(11, LOW);
+  delay(1000);
+
+  digitalWrite(12, HIGH);
+  delay(1000);
+  digitalWrite(12, LOW);
   delay(1000);
 
 }
