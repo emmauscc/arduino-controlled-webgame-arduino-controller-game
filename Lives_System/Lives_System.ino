e136124a043b16aa6457d0c5e05c3a975f4604ab
Jacob FONTANAROSA <jacob.fontanarosa@students.emmauscc.sa.edu.au>
2021-06-29 12:34:27 +0930

    LED Lives with buttons

--- Lives_System/Lives_System.ino
+++ Lives_System/Lives_System.ino
@@ -1,43 +1,97 @@
+int buttonLives = 0;
+int buttonHit = 0;
+int lives = 4;
+
 void setup() {
-  
+  Serial.begin(9600);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(4, INPUT);
   pinMode(3, INPUT);
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, HIGH);
 }
 
 void loop() {
-
-  buttonState = digitalRead(buttonPin);
+      
+  
+  buttonLives = digitalRead(4);
+  buttonHit = digitalRead(3);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(9, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(9, LOW);
+
+  if (buttonLives == HIGH) { //when livebutton is pressed
+    if(lives<4){
+      lives=lives+1;
+    }
+      
+    Serial.print(lives);
+    if(lives==0){
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+    }else if (lives==1){
+      digitalWrite(9, HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+    }else if (lives==2){
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+    }else if (lives==3){
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, LOW);
+    }else if (lives==4){
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, HIGH);
+    }
+    delay(1000);
+  }
+
+  if (buttonHit == HIGH) { // When hitbutton is pressed lose a life
+    if(lives>0){
+      lives=lives-1;
+    }
+    
+    Serial.print(lives);
+    if(lives==4){
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, HIGH);
+    }else if (lives==3){
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, LOW);
+    }else if (lives==2){
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+    }else if (lives==1){
+      digitalWrite(9, HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+    }else if (lives==0){
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+    }
+      delay(1000);
   }
   
-  digitalWrite(9, HIGH);
-  delay(1000);
-  digitalWrite(9, LOW);
-  delay(1000);
-
-  digitalWrite(10, HIGH);
-  delay(1000);
-  digitalWrite(10, LOW);
-  delay(1000);
-
-  digitalWrite(11, HIGH);
-  delay(1000);
-  digitalWrite(11, LOW);
-  delay(1000);
-
-  digitalWrite(12, HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
-  delay(1000);
-
 }
